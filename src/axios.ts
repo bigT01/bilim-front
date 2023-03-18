ccb6bc52cb30e2b9774b40cb4ac6354b7a1e2952
Tanat <tanat8511@gmail.com>
2023-03-18 09:57:49 +0600

    feat: http to https

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosRequestConfig} from "axios";
 
 const instance = axios.create({
-    baseURL: "http://bilimplace.kz:4444/api"
+    baseURL: "https://bilimplace.kz:4444/api"
 })
 
 instance.interceptors.request.use((config) => {
